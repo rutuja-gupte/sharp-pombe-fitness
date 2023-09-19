51c5050d37fd938e730ed0c79c69aefb17243335
Rutuja Gupte <rmgupte@wisc.edu>
2023-09-19 18:16:24 -0500

    data cleaning and relative fitness histogram

--- MA Fitness.R
+++ MA Fitness.R
@@ -1,3 +1,5 @@
+# Author: Dr Nathaniel Sharp
+
 # this fits a spline to y=OD versus x=time, and then finds the maximum slope
 # code by 
 spline.slope<-function(x, y, n=101, eps=1e-5, span=0.2){
@@ -10,22 +12,44 @@ nderiv <- function(fit, x, eps=1e-5){
 
 
 # load the file that indicates treatments in different wells
-d96 <- read.csv("Trial96 2 ref.csv")
-d384 <- read.csv("Trial384 2 ref.csv")
+d <- read.csv("Ref 04 06.csv")
 
 # load the OD data
-assay96.data <- read.delim("Trial96 2 - Copy (2).txt")
-assay384.data <- read.delim("Trial384 3 - Copy.txt")
+assay.data <- read.delim("Rutuja 04 10.txt")
 
 # changing the format of time to be quarter hour increments
-assay96.data$Time<-seq(from=0.25,by=0.25,length.out=nrow(assay96.data))
-assay384.data$Time<-seq(from=0.25,by=0.25,length.out=nrow(assay384.data))
+assay.data$Time<-seq(from=0.25,by=0.25,length.out=nrow(assay.data))
 
 # calculate slopes
-d96$slope <- sapply(c(1:nrow(d96)),function(r){
-  spline.slope(assay96.data$Time, assay96.data[,which(names(assay96.data)==d96$Well[r])])
-})
-d384$slope <- sapply(c(1:nrow(d384)),function(r){
-  spline.slope(assay384.data$Time, assay384.data[,which(names(assay384.data)==d384$Well[r])])
+d$slope <- sapply(c(1:nrow(d)), function(r){
+  spline.slope(assay.data$Time, assay.data[,which(names(assay.data)==d$well[r])])
 })
 
+#Author: Rutuja Gupte
+
+library(dplyr)
+
+df <- d %>%
+  group_by(treatment) %>% 
+  summarise(avg = mean(slope))
+
+ancestors <- c('H1', 'H2', 'H3', 'D1', 'D2', 'D3')
+ancestors.haploid <- c('H1', 'H2', 'H3')
+ancestors.diploid <- c('D1', 'D2', 'D3')
+avg.haploid <- as.numeric(df %>% filter(treatment %in% ancestors.haploid) %>% summarize(avg = mean(avg)))
+avg.diploid <- as.numeric(df %>% filter(treatment %in% ancestors.diploid) %>% summarize(avg = mean(avg)))
+
+
+trial <- df %>% mutate(fit = ifelse(treatment == 'Blank', 0, ifelse(treatment %in% ancestors, 1,
+                ifelse(as.numeric(treatment) %% 2 == 0, avg/avg.diploid,
+                avg/avg.haploid)))) %>% mutate(category = ifelse(treatment == 'Blank', 0, 
+                  ifelse(treatment %in% ancestors.haploid, 'Haploid',
+                  ifelse(treatment %in% ancestors.haploid, 'Diploid',
+                  ifelse(as.numeric(treatment) %% 2 == 0, 'H', 'D')))))
+
+ggplot(trial %>% filter(category %in% c('H', 'D'))) +
+  geom_histogram(aes(x=fit), color='black') +
+  facet_grid(rows = vars(category)) + 
+  geom_vline(xintercept=1, color='red')
+
+trial %>% filter(category %in% c('H', 'D')) %>% summarise(mean(fit))
